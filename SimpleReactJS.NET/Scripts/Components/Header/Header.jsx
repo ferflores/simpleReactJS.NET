d8c75e0e970059bb26f6ee9cae04fe6f8c7af3db
ferflores <ferflores@gmail.com>
2016-02-02 22:43:56 -0600

    add TODO List text to header

--- SimpleReactJS.NET/Scripts/Components/Header/Header.jsx
+++ SimpleReactJS.NET/Scripts/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
 			<nav className="navbar navbar-default">
 			  <div className="container-fluid">
 				<div className="navbar-header">
-				  <a className="navbar-brand" href="#">SimpleReactJS.NET</a>
+				  <a className="navbar-brand" href="#">SimpleReactJS.NET TODO List</a>
 				</div>
 			  </div>
 			</nav>
